a071aefa771b57a0e8b413be1da10e7c4ec66589
j._.jinoo <jinoo0306@naver.com>
2024-07-03 06:05:29 +0900

    feat : 로그인 API 기능 구현

--- member/views.py
+++ member/views.py
@@ -1,10 +1,12 @@
+# views.py
+
 from django.shortcuts import render
-from rest_framework import status, serializers
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Member
-from .serializers import MemberSerializer, MemberDetailSerializer
+from .serializers import MemberSerializer, MemberDetailSerializer, MemberLoginSerializer
 
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
@@ -13,6 +15,7 @@ from datetime import datetime
 import logging
 
 logger = logging.getLogger(__name__)
+
 
 class MemberSignUpView(APIView):
     @swagger_auto_schema(
@@ -66,46 +69,9 @@ class MemberSignUpView(APIView):
                 return Response(response_data, status=400)
         logger.warning(f'WARNING {client_ip} {current_time} POST /members 400 signup failed')
         return Response(serializer.errors, status=400)
-    
+
+
 class MemberDetailView(APIView):
-    
-    @swagger_auto_schema(
-        operation_summary="회원 정보 조회 API",
-        operation_description="이 API는 특정 회원의 정보를 조회하는 데 사용됩니다.",
-        responses={
-            200: openapi.Response(
-                description="회원 정보 조회 성공",
-                examples={
-                    "application/json": {
-                        "code": "P001",
-                        "status": 200,
-                        "message": "회원 정보 조회 성공",
-                        "data": {
-                            "login_id": "string",
-                            "nickname": "string",
-                            "age": "integer",
-                            "sex": "string",
-                            "country": "string",
-                            "code": "P001",
-                            "HTTPstatus": 200,
-                            "message": "회원 정보 조회 성공"
-                        }
-                    }
-                }
-            ),
-            404: openapi.Response(
-                description="회원 정보가 없습니다.",
-                examples={
-                    "application/json": {
-                        "code": "P002",
-                        "status": 404,
-                        "message": "회원 정보가 없습니다."
-                    }
-                }
-            ),
-        }
-    )
-        
     @swagger_auto_schema(
         operation_summary="회원 정보 수정 API",
         operation_description="이 API는 특정 회원의 정보를 수정하는 데 사용됩니다.",
@@ -155,7 +121,7 @@ class MemberDetailView(APIView):
                 "message": "회원 정보가 없습니다."
             }
             logger.warning(f'WARNING {client_ip} {current_time} PATCH /members 404 does not existing')
-            return Response(response_data,status=404)
+            return Response(response_data, status=404)
         serializer = MemberDetailSerializer(instance=member, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
@@ -165,5 +131,56 @@ class MemberDetailView(APIView):
                 "message": "회원 정보 수정 완료"
             }
             logger.info(f'INFO {client_ip} {current_time} PATCH /members/{member_id} 200 update success')
-            return Response(response_data,status=200)
+            return Response(response_data, status=200)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MemberLoginView(APIView):
+    @swagger_auto_schema(
+        operation_summary="로그인 API",
+        operation_description="이 API는 사용자 로그인을 처리하는 데 사용됩니다.",
+        request_body=MemberLoginSerializer,
+        responses={
+            200: openapi.Response(
+                description="로그인 성공",
+                examples={
+                    "application/json": {
+                        "code": "A002",
+                        "status": 201,
+                        "message": "로그인 성공"
+                    }
+                }
+            ),
+            400: openapi.Response(
+                description="로그인 실패",
+                examples={
+                    "application/json": {
+                        "code": "L002",
+                        "status": 400,
+                        "message": "로그인 실패"
+                    }
+                }
+            ),
+        }
+    )
+    def post(self, request):
+        client_ip = request.META.get('REMOTE_ADDR', None)
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        serializer = MemberLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            login_id = serializer.validated_data['login_id']
+            member = Member.objects.get(login_id=login_id)
+            response_data = {
+                "code": "L001",
+                "status": 200,
+                "message": "로그인 성공"
+            }
+            logger.info(f'INFO {client_ip} {current_time} POST /members/login 200 login success')
+            return Response(response_data, status=200)
+        response_data = {
+            "code": "L002",
+            "status": 400,
+            "message": "로그인 실패"
+        }
+        logger.warning(f'WARNING {client_ip} {current_time} POST /members/login 400 login failed')
+        return Response(response_data, status=400)
