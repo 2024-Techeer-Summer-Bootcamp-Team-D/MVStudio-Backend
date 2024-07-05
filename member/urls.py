71f77978c5c43130510ce020d672e54ef827de77
kimzini <rlawlsdl11111@gmail.com>
2024-07-06 00:10:26 +0900

    feat : 국가 리스트 조회 기능 구현

--- member/urls.py
+++ member/urls.py
@@ -1,7 +1,7 @@
 # member/urls.py
 
 from django.urls import path
-from .views import MemberSignUpView,MemberDetailView, MemberLoginView
+from .views import MemberSignUpView,MemberDetailView, MemberLoginView, CountryListView
 
 
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', MemberSignUpView.as_view(), name='member-sign-up'),
     path('<int:member_id>/',MemberDetailView.as_view(), name='member-detail'),
     path('login/', MemberLoginView.as_view(), name='member-login'),
+    path('countries/', CountryListView.as_view(), name='country-list'),
 ]
