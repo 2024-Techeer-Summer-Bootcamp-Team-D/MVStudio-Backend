10b6deb105c00413c8cb18de1f97ba17bea28e70
GiHwan2 <sandwich5011@naver.com>
2024-07-19 01:27:16 +0900

    fix: url 엔드포인트 수정 및 password 재확인 로직 삭제

--- member/serializers.py
+++ member/serializers.py
@@ -7,25 +7,10 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
-def validate_password12(password1, password2):
-
-    if not password1 or not password2:
-        raise serializers.ValidationError(
-            _("need two password fields")
-        )
-    if password1 != password2:
-        raise serializers.ValidationError(
-            _("password fields didn't match!"))
-
-    return password1
-
-
 class RegisterSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=150, write_only=True)
     email = serializers.EmailField(write_only=True)
-    password1 = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
 
     def validate_email(self, email):
         if not email:
@@ -54,7 +39,7 @@ class RegisterSerializer(serializers.Serializer):
         return username
 
     def validate(self, data):
-        data['password1'] = validate_password12(data['password1'], data['password2'])
+        data['password'] = data['password']
         data['email'] = self.validate_email(data['email'])
         data['username'] = self.validate_username(data['username'])
         print("check validate ALL")
@@ -64,7 +49,7 @@ class RegisterSerializer(serializers.Serializer):
     def get_cleaned_data(self):
         return {
             'username': self.validated_data.get('username', ''),
-            'password1': self.validated_data.get('password1', ''),
+            'password': self.validated_data.get('password', ''),
             'email': self.validated_data.get('email', '')
         }
 
@@ -72,7 +57,7 @@ class RegisterSerializer(serializers.Serializer):
         user = User.objects.create_user(
             username=validated_data["username"],
             email=validated_data["email"],
-            password=validated_data["password1"]
+            password=validated_data["password"]
         )
 
         return user
--- member/urls.py
+++ member/urls.py
@@ -5,10 +5,10 @@ from .views import *
 
 
 urlpatterns = [
-    path('sign-up', UserCreateApi.as_view(), name='memeber-create'),
-    path('login', LoginApi.as_view(), name='login'),
-    path('logout', LogoutApi.as_view(), name='login'),
-    path('countries', CountryListView.as_view(), name='countries-list'),
-    path('refresh', RefreshJWTtoken.as_view(), name='memeber-refresh'),
-    path('details/<str:username>', MemberDetailView.as_view(), name='member-detail'),
+    path('/sign-up', UserCreateApi.as_view(), name='memeber-create'),
+    path('/login', LoginApi.as_view(), name='login'),
+    path('/logout', LogoutApi.as_view(), name='login'),
+    path('/countries', CountryListView.as_view(), name='countries-list'),
+    path('/refresh', RefreshJWTtoken.as_view(), name='memeber-refresh'),
+    path('/details/<str:username>', MemberDetailView.as_view(), name='member-detail'),
 ]
