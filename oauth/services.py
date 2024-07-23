e0c77b1cec507c9acc9f52e3f2b7afbea24b6b18
GiHwan2 <sandwich5011@naver.com>
2024-07-23 21:45:30 +0900

    youtube 동영상 업로드 기능 구현

--- oauth/services.py
+++ oauth/services.py
@@ -2,17 +2,17 @@ import requests
 
 from django.conf import settings
 from django.core.exceptions import ValidationError
+from music_videos.models import MusicVideo
 
 GOOGLE_ACCESS_TOKEN_OBTAIN_URL = 'https://oauth2.googleapis.com/token'
 GOOGLE_USER_INFO_URL = 'https://www.googleapis.com/oauth2/v3/userinfo'
 
 
-def google_get_access_token(google_token_api, code):
+def google_get_access_token(google_token_api, code, redirection_uri):
     client_id = settings.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY
     client_secret = settings.SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET
     code = code
     grant_type = 'authorization_code'
-    redirection_uri = settings.BASE_BACKEND_URL + "api/v1/oauth/login/google/callback"
     state = "random_string"
 
     google_token_api += \
@@ -26,7 +26,6 @@ def google_get_access_token(google_token_api, code):
     access_token = token_response.json().get('access_token')
 
     return access_token
-
 
 def google_get_user_info(access_token):
     user_info_response = requests.get(
@@ -42,3 +41,43 @@ def google_get_user_info(access_token):
     user_info = user_info_response.json()
 
     return user_info
+
+
+def download_video(video_url):
+    response = requests.get(video_url, stream=True)
+    response.raise_for_status()  # 요청이 성공적으로 완료되지 않으면 예외 발생
+    return response.raw
+def google_upload_youtube(access_token, mv_id):
+    mv = MusicVideo.objects.get(id=mv_id)
+
+    url = "https://www.googleapis.com/upload/youtube/v3/videos?part=snippet,status"
+
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "video/*"
+    }
+
+    body = {
+        "snippet": {
+            "title": mv.subject,
+            "description": f"{mv.username}\'s music video",
+            "tags": [mv.instrument_id.name, mv.genre_id.name, mv.tempo, mv.language, mv.vocal, mv.style_id.name],
+            "categoryId": 22
+        },
+        "status": {
+            "privacyStatus": "public"
+        }
+    }
+
+    video_data = download_video(mv.mv_file)
+
+    # Upload
+    response = requests.post(url, headers=headers, json=body, data=video_data)
+
+    if response.status_code != 200:
+        raise Exception(f"Failed to upload metadata: {response.json()}")
+
+    if response.status_code != 200:
+        raise Exception(f"Failed to upload video: {response.json()}")
+
+    print(f"Upload successful! Video ID: {response.json()['id']}")
--- oauth/urls.py
+++ oauth/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('/login/google', views.LoginGoogleView.as_view(), name='login_auth_google'),
     path('/login/google/callback', views.LoginGoogleCallbackView.as_view(), name='login_google_callback'),
-    path('/youtube', views.AuthYoutubeView.as_view(), name='youtube_auth'),
+    path('/youtube/<int:mv_id>', views.YoutubeUploadGoogleView.as_view(), name='youtube_auth'),
     path('/instagram', views.AuthYoutubeView.as_view(), name='instagram_auth'),
-    path('/youtube/callback', views.AuthYoutubeCallbackView.as_view(), name='youtube_callback'),
+    path('/youtube/callback', views.YoutubeUploadGoogleCallbackView.as_view(), name='youtube_callback'),
     path('/instagram/callback', views.AuthYoutubeCallbackView.as_view(), name='instagram_callback'),
 ]
