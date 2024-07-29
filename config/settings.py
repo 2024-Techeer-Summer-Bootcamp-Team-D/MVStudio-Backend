f37acf0e65ad9a1436894fb71dce597e20655fa3
kimzini <rlawlsdl11111@gmail.com>
2024-07-30 00:05:42 +0900

    merge : develop merge

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'EXCEPTION_HANDLER': 'oauth.handler.custom_exception_handler',
+    # 'EXCEPTION_HANDLER': 'oauth.handler.custom_exception_handler',
 }
 
 
